7065232d315d29a4514ed9ee51465474c2deb624
Kevin Cowan <kevin@kevincowan.com>
2020-05-20 01:15:41 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+ function clearAllInputs() {
+   $(':input').val('');
+ }
+
+$(function() {
+  /*
+if (window.location.protocol != "https:") {
+  var url = window.location.href.substring(window.location.protocol.length);
+  alert()
+  window.location.href = "https:" + ;
+}
+    */
+})
